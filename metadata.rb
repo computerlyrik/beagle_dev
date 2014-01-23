48f762c8042b868667a302166fe686dde9586973
Christian Fischer <sw-dev@computerlyrik.de>
2014-01-23 19:26:03 +0100

    added metadata to main cookbook

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'beagle_dev'
+maintainer       'computerlyrik'
+maintainer_email 'chef-cookbooks@computerlyrik.de'
+license          'Apache 2.0'
+description      'Installs/Configures foo2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'beagle_dev'
+supports 'ubuntu'
